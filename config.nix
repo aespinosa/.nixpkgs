2114d6b83dbd241b27070acc271f17feefe0d15a
Allan Espinosa <allan.espinosa@outlook.com>
2016-03-08 22:25:38 -0600

    Try out Jenkins 2.0-alpha
    OMG!!!

--- config.nix
+++ config.nix
@@ -27,13 +27,23 @@
       '';
     };
 
+    jenkins2 = stdenv.mkDerivation {
+      name = "jenkins-2.0-alpha";
+      src = fetchurl {
+        url = "http://mirrors.jenkins-ci.org/war-rc/2.0/jenkins.war";
+        sha256 = "133r6z38gmllr7373gz85j9k5c7gdiz9p8c072dkrmwfzyxf51jx";
+      };
+
+      buildCommand = "ln -sf $src $out";
+    };
+
     plistService = callPackage ./plist.nix {};
 
     logDir = "/usr/local/var/log";
 
     jenkinsService = plistService {
       name = "jenkins";
-      programArgs = [ "java" "-jar" "${jenkins}" ];
+      programArgs = [ "java" "-jar" "${jenkins2}" ];
       stdout = "${logDir}/jenkins.log";
       stderr = "${logDir}/jenkins.log";
     };
