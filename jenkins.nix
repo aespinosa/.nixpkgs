4ccf1158110284467bb4328fa57dc8e718ea107c
Allan Espinosa <allan.espinosa@outlook.com>
2016-04-07 17:51:04 -0500

    Bump Jenkins 2.0 to rc1

--- jenkins.nix
+++ jenkins.nix
@@ -6,10 +6,10 @@ let
   logDir = "/usr/local/var/log";
   jenkins2 = stdenv.mkDerivation rec {
     name = "jenkins-${version}";
-    version = "2.0-beta-2";
+    version = "2.0-rc-1";
     src = fetchurl {
       url = "http://repo.jenkins-ci.org/public/org/jenkins-ci/main/jenkins-war/${version}/jenkins-war-${version}.war";
-      sha256 = "1nhz99fzgnm8v8s6p4wb7fry9495mwrzbivpmp5yj2aj43wpxick";
+      sha256 = "14ahpz3613pbzsi8mxhj5c5lfv96a5w89ibr2mrqbw7rarw0hbfr";
     };
 
     buildCommand = "ln -sf $src $out";
