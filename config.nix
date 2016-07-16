84544fa3ec11c4ae47d4efadf9055c529442c675
Allan Espinosa <allan.espinosa@outlook.com>
2016-07-16 11:29:27 -0500

    Upgrade google-cloud-sdk

--- config.nix
+++ config.nix
@@ -132,8 +132,8 @@
     google-cloud-sdk = stdenv.mkDerivation {
       name = "google-cloud-sdk-110.0.0";
       src = fetchurl {
-        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-110.0.0-darwin-x86_64.tar.gz";
-        sha256 = "0fpz2dvs8by1pycf1q5lnwh0k5q7d29ilzb9wnzmx8kgq2716nh4";
+        url = "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-117.0.0-darwin-x86_64.tar.gz";
+        sha1 = "be176b412a861ba1d7399088f6c77c513d3dfb13";
       };
 
       buildCommand = ''
