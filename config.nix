9b088517bbd8ba0df3023c96ca862f5ef24d1b2e
Allan Espinosa <allan.espinosa@outlook.com>
2017-04-26 21:02:39 -0500

    Remove haskell hacks

--- config.nix
+++ config.nix
@@ -9,31 +9,6 @@
       paths = [ screen gitMinimal git-lfs tig ack zsh irssi ];
     };
 
-    staticHaskellPackages = haskellPackages.override {
-      overrides = self: super: {
-        mkDerivation = args: super.mkDerivation (args // {
-          #enableSharedExecutables = false;
-          #enableSharedLibraries = false;
-          postCompileBuildDriver = ''
-            echo "Patching dynamic library dependencies"
-            # 1. Link all dylibs from 'dynamic-library-dirs's in package confs to $out/lib/links
-            mkdir -p $out/lib/links
-            for d in $(grep dynamic-library-dirs $packageConfDir/*|awk '{print $2}'); do
-              ln -s $d/*.dylib $out/lib/links
-            done
-                
-            # 2. Patch 'dynamic-library-dirs' in package confs to point to the symlink dir
-            for f in $packageConfDir/*.conf; do
-              sed -i "s,dynamic-library-dirs: .*,dynamic-library-dirs: $out/lib/links," $f
-            done
-
-            # 3. Recache package database
-            ghc-pkg --package-db="$packageConfDir" recache
-         '';
-        });
-      };
-    };
-
     macvim = stdenv.mkDerivation {
       name = "macvim-122";
       src = fetchurl {
